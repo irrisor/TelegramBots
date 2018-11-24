a52a23bee0e188167bdf762b06efd4cdcc12fcfd
Irrisor <irrisor@users.noreply.github.com>
2018-11-24 13:11:44 +0100

    allow to shut down the sessions

--- telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotInitializer.java
+++ telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotInitializer.java
@@ -1,45 +1,61 @@
 package org.telegram.telegrambots.starter;
 
-import java.util.List;
-import java.util.Objects;
-
 import org.springframework.beans.factory.InitializingBean;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.meta.generics.WebhookBot;
+
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Receives all beand which are #LongPollingBot and #WebhookBot and register them in #TelegramBotsApi.
  */
 public class TelegramBotInitializer implements InitializingBean {
 
-	private final TelegramBotsApi telegramBotsApi;
+    private final TelegramBotsApi telegramBotsApi;
     private final List<LongPollingBot> longPollingBots;
     private final List<WebhookBot> webHookBots;
+    private List<BotSession> botSessions = new ArrayList<>();
 
     public TelegramBotInitializer(TelegramBotsApi telegramBotsApi,
-    								List<LongPollingBot> longPollingBots,
-                                    List<WebhookBot> webHookBots) {
-    	Objects.requireNonNull(telegramBotsApi);
-    	Objects.requireNonNull(longPollingBots);
-    	Objects.requireNonNull(webHookBots);
-    	this.telegramBotsApi = telegramBotsApi;
+                                  List<LongPollingBot> longPollingBots,
+                                  List<WebhookBot> webHookBots) {
+        Objects.requireNonNull(telegramBotsApi);
+        Objects.requireNonNull(longPollingBots);
+        Objects.requireNonNull(webHookBots);
+        this.telegramBotsApi = telegramBotsApi;
         this.longPollingBots = longPollingBots;
         this.webHookBots = webHookBots;
     }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		try {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        try {
             for (LongPollingBot bot : longPollingBots) {
-                telegramBotsApi.registerBot(bot);
+                addSession(telegramBotsApi.registerBot(bot));
             }
             for (WebhookBot bot : webHookBots) {
                 telegramBotsApi.registerBot(bot);
             }
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
-        }		
-	}
+        }
+    }
+
+    private void addSession(BotSession botSession) {
+
+        botSessions.add(botSession);
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        for (BotSession session : botSessions) {
+            session.stop();
+        }
+    }
 }
